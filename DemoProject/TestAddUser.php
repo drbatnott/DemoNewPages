1cb5ea13e9e65ff7034ed04207e01ce9faee15aa
drbatnott <pszpxb@exmail.nottingham.ac.uk>
2017-11-14 09:45:57 +0000

    First test - create a user entry page. Passes

---
+++ DemoProject/TestAddUser.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+<?php
+/**
+* Test Add User
+* User strory: New User will add data to form and submit
+* First step is to produce form
+*/
+?>
+<form>
+<!-- What should it look like?  Table-->
+	<table>
+		<tr>
+			<th>Username</th>
+		</tr>
+		<tr>
+			<td><input type="text" name="username"/></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
